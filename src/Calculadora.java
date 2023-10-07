a8b405e8e2acd52035e8a88749a3236ec0719cc5
jujis <jujis@jujise96-laptop>
2023-10-07 17:15:46 +0200

    Clase calculadora creada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,27 @@
 
 public class Calculadora {
+	
+	public static int suma (int Valor1, int Valor2)
+	{
+		return (Valor1+Valor2);
+	}
+	
+	public static int resta(int Valor1, int Valor2)
+	{
+		return(Valor1-Valor2);
+	}
+	
+	public static int multiplica(int Valor1, int Valor2)
+	{
+		return(Valor1*Valor2);
+	}
+	
+	public static int divide(int Valor1, int Valor2)
+	{
+		if(Valor2 == 0 || Valor1 == 0)
+			return -1;
+		else
+			return (Valor1/Valor2);
+	}
 
 }
